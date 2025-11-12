92ac191e03fca87f92704643f242070960e85937
mariammashoud <mariammashoud0@gmail.com>
2025-11-12 15:38:25 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -2,6 +2,41 @@
 #define ENCLOSURE_H
 
 #include "Animal.h"
+#include "animal.h"
+#include "Mammal.h"
+
+
+class Bird : public animal {
+private:
+    float wingSpan;
+
+public:
+
+    Bird() : animal() {
+        wingSpan = 0.0;
+    }
+
+
+    Mammal(string W) : animal(n, a) {
+        furColor = W;
+    }
+
+
+    ~Mammal() {
+
+    }
+
+
+    void setWingspan(int W){
+        wingSpan= W;
+    }
+
+
+
+    float getWingSpan() {
+        return wingSpan;
+    }
+};
 
 //define enclosure class here 
 
