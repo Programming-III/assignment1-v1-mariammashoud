ca6eb29a2948b7ccf22490ed613c3372d00f2b58
mariammashoud <mariammashoud0@gmail.com>
2025-11-12 15:46:59 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -6,7 +6,57 @@ using namespace std;
 
 //define visitor class here
 
+#include <string>
 
+class Visitor {
+private:
+    string VisitorName;
+    int ticketsBought;
+
+
+public:
+
+    Visitor(string name, int ticketsBought){
+        VisitorName = name;
+        this->ticketsBought = ticketsBought;
+
+    }
+
+
+
+
+
+    ~Visitor(){
+
+    }
+
+
+   void setVis
+
+
+    string getName() {
+        return name;
+    }
+    int getAge() {
+        return age;
+    }
+
+
+    virtual void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+    void feed() {
+        if (isHungry) {
+            cout << "Hungry" << endl;
+        }
+        else {
+            cout << "Not hungry" << endl;
+        }
+    }
+};
+
+#e
 
 
 
