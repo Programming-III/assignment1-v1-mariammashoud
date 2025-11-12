bbae4f4b3cf995c62afdf495893a4265ba7eb9a3
mariammashoud <mariammashoud0@gmail.com>
2025-11-12 15:37:37 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -5,6 +5,41 @@
 
 //define class bird here 
 
+#include "animal.h"
+#include "Mammal.h"
+
+
+class Bird : public animal {
+private:
+    float wingSpan;
+
+public:
+
+    Bird() : animal() {
+        wingSpan = 0.0;
+    }
+
+
+    Mammal(string W) : animal(n, a) {
+        furColor = W;
+    }
+
+
+    ~Mammal() {
+
+    }
+
+
+    void setWingspan(int W){
+        wingSpan= W;
+    }
+
+
+
+    float getWingSpan() {
+        return wingSpan;
+    }
+};
 
 
 
