4ac7adeab657d6702e9ff37196819a9ed2e412e0
mariammashoud <mariammashoud0@gmail.com>
2025-11-12 15:40:46 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -2,7 +2,43 @@
 #define MAMMAL_H
 
 #include "Animal.h"
+include "animal.h"
 
+
+class furColor : public animal {
+private:
+    string furColor;
+
+public:
+
+    Mammal() : animal() {
+        furColor = null;
+    }
+
+
+    Mammal(string F) : animal(n, a) {
+        furColor = "F";
+    }
+
+
+    ~Mammal() {
+
+    }
+
+
+    void setfurColor(int F){
+    furColor= F;
+    }
+
+
+
+    int getfurColor() {
+        return furColor;
+    }
+
+
+
+};
 //define mammal class here
 
 
