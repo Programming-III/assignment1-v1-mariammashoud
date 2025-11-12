1aaaa4c42e31250f977629f39756f3306ac74153
mariammashoud <mariammashoud0@gmail.com>
2025-11-12 15:44:17 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -2,6 +2,28 @@
 #define REPTILE_H
 
 #include "Animal.h"
+//
+// Created by maria on 12/11/2025.
+//
+
+#ifndef ANIMAL_REPLITE_H
+#define ANIMAL_REPLITE_H
+
+
+class Replite: public animal {
+private:
+    bool isVenomous = false;
+
+
+    ~Replite() {
+
+    }
+};
+
+
+
+
+
 
 //define reptile class here
 
